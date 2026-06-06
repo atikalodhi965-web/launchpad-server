25bcd22a4fcdad1d003e4341142bde87374e5478
Atikahamid <hafizaatikahamid965@gmail.com>
2026-06-06 15:30:54 +0500

    otp7

--- src/services/otpService.ts
+++ src/services/otpService.ts
@@ -20,7 +20,7 @@ const transporterConfig: any = isGmail ? {
 } : {
   host: process.env.EMAIL_HOST,
   port: parseInt(process.env.EMAIL_PORT || '587'),
-  secure: process.env.EMAIL_PORT === '465',
+  // secure: process.env.EMAIL_PORT === '465',
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
